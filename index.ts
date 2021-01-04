6d362349326ae31b1b33978bb2e862dc26f9a8ad
Lukas <lukasdobbles@gmail.com>
2021-01-04 14:41:15 -0800

    exported 'request' and 'Response'

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import get from './src/get'
 import post from './src/post'
+import request from './src/request'
+import Response from './src/response'
 
 export = {
     get,
-    post
+    post,
+    request,
+    Response
 }
